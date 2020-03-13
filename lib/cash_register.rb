623c507514cfc85be85174656dd072200f7e852a
Laila Campbell <lailacampbell96@gmail.com>
2020-03-13 03:23:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ end
 
 def apply_discount
   if @discount > 0 
-  disc_total = (@toatl * (discount.to_f/100.to_f))
+  disc_total = (@total * (discount.to_f/100.to_f))
   @total -= disc_total
   return "After the discount, the total comes to $#{@total.to_i}."
 else 
