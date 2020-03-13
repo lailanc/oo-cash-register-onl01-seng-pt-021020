994cd9626f7b4184a8f5256a82c4f2a73edf4b4c
Laila Campbell <lailacampbell96@gmail.com>
2020-03-13 03:09:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,24 @@
 class CashRegsiter
- def initialize (total) 
-   @total = total
+  attr_accessor :total, :discount, :items
+  
+ def initialize (discount = 0) 
+   @total = 0 
+   @discount = discount
+   @items = []
  end
+ 
+ def add_item (title, price, quanity = 1)
+  @total += price * quanity
+  quanity.times do
+    @items << title 
+  end 
+  @last_item = price * quanity
+end 
+
+def apply_discount
+  if @discount > 0 
+    
+   
  
   
   
